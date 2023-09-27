41c27304ea820ea6114282c694fb234322284b94
Jake Liu <13583702+LittleJake@users.noreply.github.com>
2023-09-27 13:13:34 +0800

    Update webget.cpp

--- src/handler/webget.cpp
+++ src/handler/webget.cpp
@@ -94,7 +94,7 @@ public:
 RWLock cache_rw_lock;
 
 //std::string user_agent_str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
-static std::string user_agent_str = "subconverter/" VERSION " cURL/" LIBCURL_VERSION;
+static std::string user_agent_str = "subconverter/" VERSION " cURL/" LIBCURL_VERSION " Clash cfw";
 
 struct curl_progress_data
 {
